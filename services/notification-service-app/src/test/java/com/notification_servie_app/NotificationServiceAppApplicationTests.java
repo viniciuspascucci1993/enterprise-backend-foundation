02040e8143192a352a889144e34a634778bfbcc7
viniciuspascucci1993 <vinicius.pascucci1@gmail.com>
2026-02-25 10:31:31 -0300

    chore: initial notification-service-app bootstrap structure

---
+++ services/notification-service-app/src/test/java/com/notification_servie_app/NotificationServiceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notification_servie_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
